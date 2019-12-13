dcbcfc06a0fcad6cc4c59768735551ed2064c71a
Anoop-Shah <58204227+Anoop-Shah@users.noreply.github.com>
2019-12-13 14:24:33 +0530

    Update output.sh

--- output.sh
+++ output.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 echo "Waiting for Database to start ......"
-sleep 30
+sleep 10
 /usr/bin/python /opt/sample/cgi-bin/first.py
+
+echo "Starting Web server"
+sleep 5
+cd /opt/sample
+python -m SimpleHTTPServer
